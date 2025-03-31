d5bb4936489fd9f714e85e90d872e027b4b5037d
Kobe <87835153+KbeDemy@users.noreply.github.com>
2025-03-31 13:07:23 +0200

    Update Project_main.ino

--- Project_main/Project_main.ino
+++ Project_main/Project_main.ino
@@ -3,188 +3,162 @@
 #include <HX711_ADC.h> // library voor ADC converter LOADCELL
 #include <EEPROM.h> // opslaan van cali val voor LOADCELL
 
-#define stepPin 2 // step pin
-#define dirPin 3 // dir pin
-#define downButtonPin 8   // Knop voor omhoog
-#define upButtonPin 9 // Knop voor omlaag
-#define HX711_dout 5 //mcu > HX711 dout pin
-#define HX711_sck 6 //mcu > HX711 sck pin
-#define startButtonPin 7 // Start knop voor automatische modus
-#define stopButtonPin 12 // Stop knop voor automatische modus
-#define pumpPin 11 // Pin voor de pomp (NC geschakeld)
-#define valvePin 10 // Pin voor de klep
 
+
+// pin def
+const uint8_t stepPin  = 9; 
+const uint8_t dirPin = 10; 
+
+const uint8_t joystickPin = A1; // Y 
+  
+const uint8_t startPin = 4; 
+const uint8_t stopPin = 2; 
+
+const uint8_t HX711_dout = 6;
+const uint8_t HX711_sck = 7;
+
+const uint8_t pumpPin = 11; // (NC)
+const uint8_t valvePin = 12; 
+
+
+
+// global objects
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
 
-const int calVal_calVal_eepromAdress = 0;
+// global var
+const int serialPrintInterval = 50; // increase value to slow down serial print activity
+const float newtonMax = 10.0; // Max force
+
 unsigned long t = 0;
 static boolean newDataReady = 0;
-const int serialPrintInterval = 50; // increase value to slow down serial print activity
-float NewtonValue = 0;
-int stepDelay = 1000;
 
-// Nieuwe variabelen voor automatische modus
-const float newtonMax = 10.0; // Maximale kracht in Newton
-bool isAutomaticMode = false;
-bool isPumpRunning = false;
-bool isValveOpen = false;
+float newtonValue = 0;
 unsigned long pumpStartTime = 0;
-const unsigned long PUMP_DURATION = 15000; // 1 minuut in milliseconds
+unsigned long pumpTime = 15000; // 1 min in millisec
 
-void setup() {
-  Serial.begin(115200);
 
+//status var
+volatile bool automaticMode = false;
+
+void setupPins() {
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
-  pinMode(upButtonPin, INPUT_PULLUP);  
-  pinMode(downButtonPin, INPUT_PULLUP);
-  pinMode(startButtonPin, INPUT_PULLUP);
-  pinMode(stopButtonPin, INPUT_PULLUP);
+  pinMode(joystickPin, INPUT);
+  pinMode(startPin, INPUT_PULLUP);
+  pinMode(stopPin, INPUT_PULLUP);
   pinMode(pumpPin, OUTPUT);
   pinMode(valvePin, OUTPUT);
   pinMode(13,OUTPUT);
   // Start met pomp en klep uit
-  digitalWrite(pumpPin, HIGH); // NC geschakeld, dus HIGH is uit
+  digitalWrite(pumpPin, HIGH); // NC 
   digitalWrite(valvePin, LOW);
+  
+  attachInterrupt(digitalPinToInterrupt(stopPin), ISR_BREAK, FALLING);
+}
 
-  // initialize the loadcell
-  float calibrationValue = 231.72; // calibration value
+void setupLoadCell(){
+    float calibrationValue = 231.72; // measert with 1kg (.5 acc)
   LoadCell.begin();
-  LoadCell.start(1000, true); // functie vraag een "stabilizatie tijd en en "do tare" 
+  LoadCell.start(1000, true); // time, do tare 
   if (LoadCell.getTareTimeoutFlag()) {
     Serial.println("ERROR");
   }
   else {
-    LoadCell.setCalFactor(calibrationValue); // set calibration factor (float)
+    LoadCell.setCalFactor(calibrationValue);
     Serial.println("Startup is complete");
   }
   while (!LoadCell.update());
-  // initialize the lcd
-  lcd.init();                      
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  setupPins();
+  setupLoadCell();
+
+  lcd.init();      // initialize the LCD                
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("Starting up ...");
 }
 
 void loop() {
-  // Check voor start/stop knoppen
-  if (digitalRead(startButtonPin) == LOW && !isAutomaticMode) {
-    isAutomaticMode = true;
-    Serial.println("Automatische modus gestart");
+  Serial.print(automaticMode);
+  if (digitalRead(startPin) == LOW && !automaticMode){
+    automaticMode = true;
+    Serial.println("automatic mode has started");
+    lcd.clear();
+    lcd.setCursor(1,0);
+    lcd.print("auto");
   }
-  
-  if (digitalRead(stopButtonPin) == LOW && isAutomaticMode) // interrupt maken die programma direct stopt
-  { 
-    isAutomaticMode = false;
-    isPumpRunning = false;
-    isValveOpen = false;
-    digitalWrite(pumpPin, HIGH); // Zet pomp uit
+
+  if(!automaticMode){
+     // Serial.println("manual mode");
+    manual();
+    digitalWrite(pumpPin, HIGH); // Zet pomp UIT
     digitalWrite(valvePin, LOW); // Zet klep dicht
-    Serial.println("Automatische modus gestopt");
   }
+}
 
-  if (isAutomaticMode) {
-    automatish();
-  } else {
-    // Bestaande handmatige besturing
-    if (NewtonValue < 0) NewtonValue = -NewtonValue;
-      
-    stepDelay = 100 + (abs(NewtonValue) * 190);  // lineare herschaling naarmate dat de belasting stijgt
-    
-    if (stepDelay < 100) stepDelay = 100;     // Maximum snelheid
-    if (stepDelay > 1000) stepDelay = 1000;   // Minimum snelheid
+void ISR_BREAK(){
+  automaticMode = false;
+}
 
-    //Serial.println(stepDelay);
 
-    if (digitalRead(downButtonPin) == LOW) { 
-      Serial.println("downbutton"); 
-      digitalWrite(dirPin, HIGH);           // omhoog
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(stepDelay); // de snelheid 
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(stepDelay);
+void manual(){
+  int joystickValue = analogRead(joystickPin); 
+  int deadZone = 50; 
+
+  readLoadCell();
+  Serial.println(newtonValue);
+  if (newtonValue < newtonMax){
+    if (joystickValue > (512 + deadZone)) {  
+      int stepDelay = map(joystickValue, 512 + deadZone, 1023, 1000, 100);  // verder-> sneller
+      Serial.print("UP: "); 
+      Serial.println(stepDelay);
+      moveMotor(HIGH, stepDelay);
     } 
-    else if (digitalRead(upButtonPin) == LOW) { 
-      Serial.println("upbutton"); 
-      digitalWrite(dirPin, LOW);                   // omlaag
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(stepDelay); // de snelheid 
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(stepDelay);
+    else if (joystickValue < (512 - deadZone)) {
+      int stepDelay = map(joystickValue, 0, 512 - deadZone, 1000, 100); //
+      
+      Serial.print("   DOWN: "); 
+      Serial.println(stepDelay);
+      moveMotor(LOW, stepDelay);
+    } 
+    else {
+      Serial.println("move joystick");
+
     }
+  } 
+  else {
+    Serial.print("Maximum value reached : ");
+    Serial.println(newtonMax);
+
+    
   }
-  
-  // Load cell data reading
+}
+
+void moveMotor(bool direction, int stepDelay){
+  digitalWrite(dirPin, direction);
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(stepDelay);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(stepDelay);
+}
+
+void readLoadCell(){
   if (LoadCell.update()) newDataReady = true;  // get smoothed value from the dataset:
   if (newDataReady) {
     if (millis() > t + serialPrintInterval) {
       float GramValue = LoadCell.getData();
-      NewtonValue = GramValue / 1000 * -9.81; // waarde in gram / 1000 * 9.81 () 
-      /*
-      Serial.print("Load_cell output val in gram: ");
-      Serial.print(GramValue);
-      Serial.print("      Load_cell output in N: ");
-      Serial.println(NewtonValue);
-      */
-      UpdateLCD(NewtonValue);
+      newtonValue = GramValue / 1000 * -9.81; // waarde in gram / 1000 * 9.81 () 
       newDataReady = 0;
       t = millis();
     }
   }
 }
 
-void automatish() {
-  // Motor naar beneden laten gaan
-  //digitalWrite(dirPin, HIGH);
-  //digitalWrite(stepPin, HIGH);
-  //delayMicroseconds(stepDelay);
-  //digitalWrite(stepPin, LOW);
-  //delayMicroseconds(stepDelay);
-  if(NewtonValue <= newtonMax)
-  digitalWrite(13,HIGH);
-  // Check of maximale kracht is bereikt
-  if (NewtonValue >= newtonMax && !isPumpRunning) {
-    // Stop de motor
-    //digitalWrite(stepPin, LOW);
-      digitalWrite(13,LOW); // error moet optreden indien gewicht wegvalt tijdens process.
-    // Start de pomp
-    isPumpRunning = true;
-    digitalWrite(pumpPin, LOW); // NC geschakeld, dus LOW is aan
-    pumpStartTime = millis();
-    Serial.println("Maximale kracht bereikt, pomp gestart");
-  }
 
-  // Check of pomp lang genoeg heeft gedraaid
-  if (isPumpRunning && !isValveOpen) {
-    if (millis() - pumpStartTime >= PUMP_DURATION) {
-      // Zet pomp uit en open klep
-      digitalWrite(pumpPin, HIGH); // Zet pomp uit
-      isPumpRunning = false;
-      digitalWrite(valvePin, HIGH); // Open klep
-      isValveOpen = true;
-      Serial.println("Pomp gestopt, klep geopend");
-    }
-  }
 
-  // Debug informatie
-  if (LoadCell.update()) {
-    if (millis() > t + serialPrintInterval) {
-      Serial.print("Kracht: ");
-      Serial.print(NewtonValue);
-      Serial.print(" N | Pomp: ");
-      Serial.print(isPumpRunning ? "AAN" : "UIT");
-      Serial.print(" | Klep: ");
-      Serial.println(isValveOpen ? "OPEN" : "DICHT");
-      t = millis();
-    }
-  }
-}
-
-void UpdateLCD(float kracht){
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("kracht : ");
-  lcd.setCursor(0,1);
-  lcd.print(kracht);
-}
